215e7eb2529d8cb51f3e30024f0c1b68ba9b5150
osarumen misan <eosarumenwork@yahoo.com>
2022-07-09 12:23:47 -0400

    updated template has been completed

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
 const fs = require('fs');
+
+ const generatePage = require('./src/page-template');
+
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -130,18 +133,16 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
+     const pageHTML = generatePage();
+
     // fs.writeFile('./index.html', pageHTML, err => {
     //   if (err) throw new Error(err);
+
     //   console.log('Page created! Check out index.html in this directory to see it!');
     // });
   });
-
-
-
-// const generatePage = require('./src/page-template');
+   
+      
 
 // const pageHTML = generatePage(name, github);
 
